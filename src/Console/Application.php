f181da4983b502dc2f9198a1dd37f5b984ca45f6
Jeremy Worboys <jw@jeremyworboys.com>
2016-08-23 00:43:11 +1000

    Add additional application details

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -4,16 +4,35 @@ namespace WarbleMedia\HummingBird\Console;
 
 use Symfony\Component\Console\Application as BaseApplication;
 use WarbleMedia\HummingBird\Command;
+use WarbleMedia\HummingBird\HummingBird;
 
 class Application extends BaseApplication
 {
     /**
-     * @return \Symfony\Component\Console\Command\Command[]
+     * Application constructor.
      */
-    protected function getDefaultCommands()
+    public function __construct()
     {
-        return array_merge(parent::getDefaultCommands(), [
-            new Command\AboutCommand(),
-        ]);
+        error_reporting(-1);
+
+        parent::__construct('Humming Bird', HummingBird::VERSION);
+
+        $this->add(new Command\AboutCommand());
+    }
+
+    /**
+     * @return string
+     */
+    public function getLongVersion()
+    {
+        $version = parent::getLongVersion() . ' by <comment>Warble Media</comment>';
+        $commit = HummingBird::COMMIT_HASH;
+
+        // Need to string concat here to avoid box replacement
+        if ($commit !== '@' . 'git-commit@') {
+            $version .= ' (' . substr($commit, 0, 7) . ')';
+        }
+
+        return $version;
     }
 }
---
+++ src/HummingBird.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace WarbleMedia\HummingBird;
+
+class HummingBird
+{
+    const VERSION              = '@package_version@';
+    const COMMIT_HASH          = '@git-commit@';
+    const BRANCH_ALIAS_VERSION = '@package_branch_alias_version@';
+    const RELEASE_DATE         = '@release_date@';
+}
